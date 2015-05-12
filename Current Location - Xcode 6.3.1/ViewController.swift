009b1222ba658fd2470103bb4420234eeb2f383a
Vea Software <pj@veasoftware.com>
2015-05-12 08:25:43 -0600

    Initial Commit

---
+++ Current Location - Xcode 6.3.1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Current Location - Xcode 6.3.1
+//
+//  Created by PJ Vea on 5/12/15.
+//  Copyright (c) 2015 Vea Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
